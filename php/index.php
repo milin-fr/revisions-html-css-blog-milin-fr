3975f06bfbc39be2cdb0a1074a71a004b49bae28
milin-fr <m.ilin.fr@gmail.com>
2019-11-13 10:18:07 +0100

    html + content

---
+++ php/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../css/font.css">
+  <link rel="stylesheet" href="../css/style.css">
+  <title>Un blog tout en couleur</title>
+</head>
+
+<body>
+  <header>
+    <div id="logo_banner"></div>
+    <nav>
+      <ul class="nav-bar">
+        <li>Notre révolution</li>
+        <li>Partage de frais</li>
+        <li>Tarifs</li>
+        <li>Blog</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <article>
+      <div class="article--left_part">
+      </div>
+      <div class="article--right_part">
+        <div class="article--date">
+          Le 11 juillet 2013
+        </div>
+        <div class="article--content_container">
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, totam?
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae sint nam vero, repudiandae expedita odit
+            perspiciatis laboriosam cupiditate ex itaque minus aliquam minima. Quibusdam reiciendis nulla itaque totam
+            quae fuga, unde aperiam? Mollitia reiciendis accusamus, aspernatur hic, voluptatem voluptas sit alias in,
+            excepturi laboriosam beatae unde repudiandae! Harum iure adipisci culpa placeat aliquam repellat itaque?
+            Dicta deleniti officia obcaecati. Asperiores aspernatur eaque aliquam iure aperiam ut dolores dolore, nemo
+            maiores laudantium fugit vero delectus facere ab repellendus commodi. Officiis recusandae facere quidem
+            expedita perspiciatis soluta incidunt. Officiis omnis accusantium at quis quos nam iusto possimus molestias
+            ex? Officia, optio deleniti!
+          </p>
+        </div>
+        <div class="article--category">
+          Catégorie 3
+        </div>
+        <img class="article--social_media" src="../images/facebook.png" alt="facebook_icon">
+        <img class="article--social_media" src="../images/twitter.png" alt="twitter_icon">
+      </div>
+    </article>
+    <div class="button_container">
+      <button>
+        Precedent
+      </button>
+      <button>
+        Suivant
+      </button>
+    </div>
+  </main>
+  <footer>
+    <div class="footer--copyright">
+      <h2>Wedrive - &#9400; 2013 Tous droits réservés</h2>
+      <p>Design agence wype</p>
+    </div>
+    <ul class="footer--links">
+      <li>Mentions légales</li>
+      <li>Conditions générales</li>
+      <li>Presse</li>
+      <li>Contact</li>
+    </ul>
+  </footer>
+</body>
+
+</html>
