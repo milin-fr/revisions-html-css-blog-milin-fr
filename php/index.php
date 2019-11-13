2417a2f7f086ff23f57f574f5e0c4b258482c485
milin-fr <m.ilin.fr@gmail.com>
2019-11-13 15:43:15 +0100

    mobile first

--- php/index.php
+++ php/index.php
@@ -11,16 +11,40 @@
 </head>
 
 <body>
+  <div class="container">
   <header>
-    <div id="logo_banner"></div>
-    <nav>
-      <ul class="nav-bar">
-        <li>Notre révolution</li>
-        <li>Partage de frais</li>
-        <li>Tarifs</li>
-        <li>Blog</li>
-        <li>Contact</li>
-      </ul>
+    
+    <img src="../images/logo.png" alt="" id="logo_banner">
+    <nav class="navigation">
+      <div id="navigation--dropdown_menu">
+        <!--
+        A fake / hidden checkbox is used as click reciever,
+        so you can use the :checked selector on it.
+        -->
+        <input type="checkbox" />
+
+        <!--
+        Some spans to act as a hamburger.
+
+        They are acting like a real hamburger,
+        not that McDonalds stuff.
+        -->
+        <span></span>
+        <span></span>
+        <span></span>
+
+        <!--
+        Too bad the menu has to be inside of the button
+        but hey, it's pure CSS magic.
+        -->
+        <ul id="dropdown_menu">
+          <a href="https://ha-ha.space/"><li>Notre révolution</li></a>
+          <a href="https://ha-ha.space/"><li>About</li></a>
+          <a href="https://ha-ha.space/"><li>Info</li></a>
+          <a href="https://ha-ha.space/"><li>Contact</li></a>
+          <a href="https://ha-ha.space/"><li>Home</li></a>
+        </ul>
+      </div>
     </nav>
   </header>
   <main>
@@ -46,11 +70,112 @@
             ex? Officia, optio deleniti!
           </p>
         </div>
+        <div class="article--footer">
         <div class="article--category">
           Catégorie 3
         </div>
-        <img class="article--social_media" src="../images/facebook.png" alt="facebook_icon">
-        <img class="article--social_media" src="../images/twitter.png" alt="twitter_icon">
+        <a href="https://ha-ha.space/" target="_blank" target="_blank" class="article--social_media" ><img src="../images/facebook.png" alt="facebook_icon"></a>
+        <a href="https://ha-ha.space/" target="_blank" target="_blank" class="article--social_media" ><img src="../images/twitter.png" alt="twitter_icon"></a>
+        </div>
+      </div>
+    </article>
+    <article>
+      <div class="article--left_part">
+      </div>
+      <div class="article--right_part">
+        <div class="article--date">
+          Le 11 juillet 2013
+        </div>
+        <div class="article--content_container">
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, totam?
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae sint nam vero, repudiandae expedita odit
+            perspiciatis laboriosam cupiditate ex itaque minus aliquam minima. Quibusdam reiciendis nulla itaque totam
+            quae fuga, unde aperiam? Mollitia reiciendis accusamus, aspernatur hic, voluptatem voluptas sit alias in,
+            excepturi laboriosam beatae unde repudiandae! Harum iure adipisci culpa placeat aliquam repellat itaque?
+            Dicta deleniti officia obcaecati. Asperiores aspernatur eaque aliquam iure aperiam ut dolores dolore, nemo
+            maiores laudantium fugit vero delectus facere ab repellendus commodi. Officiis recusandae facere quidem
+            expedita perspiciatis soluta incidunt. Officiis omnis accusantium at quis quos nam iusto possimus molestias
+            ex? Officia, optio deleniti!
+          </p>
+        </div>
+        <div class="article--footer">
+        <div class="article--category">
+          Catégorie 1
+        </div>
+        <div class="article--category">
+          Catégorie 2
+        </div>
+        <a href="https://ha-ha.space/" target="_blank" class="article--social_media" ><img src="../images/facebook.png" alt="facebook_icon"></a>
+        <a href="https://ha-ha.space/" target="_blank" class="article--social_media" ><img src="../images/twitter.png" alt="twitter_icon"></a>
+        </div>
+      </div>
+    </article>
+    <article>
+      <div class="article--left_part">
+      </div>
+      <div class="article--right_part">
+        <div class="article--date">
+          Le 11 juillet 2013
+        </div>
+        <div class="article--content_container">
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, totam?
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae sint nam vero, repudiandae expedita odit
+            perspiciatis laboriosam cupiditate ex itaque minus aliquam minima. Quibusdam reiciendis nulla itaque totam
+            quae fuga, unde aperiam? Mollitia reiciendis accusamus, aspernatur hic, voluptatem voluptas sit alias in,
+            excepturi laboriosam beatae unde repudiandae! Harum iure adipisci culpa placeat aliquam repellat itaque?
+            Dicta deleniti officia obcaecati. Asperiores aspernatur eaque aliquam iure aperiam ut dolores dolore, nemo
+            maiores laudantium fugit vero delectus facere ab repellendus commodi. Officiis recusandae facere quidem
+            expedita perspiciatis soluta incidunt. Officiis omnis accusantium at quis quos nam iusto possimus molestias
+            ex? Officia, optio deleniti!
+          </p>
+        </div>
+        <div class="article--footer">
+        <div class="article--category">
+          Catégorie 1
+        </div>
+        <a href="https://ha-ha.space/" target="_blank" class="article--social_media" ><img src="../images/facebook.png" alt="facebook_icon"></a>
+        <a href="https://ha-ha.space/" target="_blank" class="article--social_media" ><img src="../images/twitter.png" alt="twitter_icon"></a>
+        </div>
+      </div>
+    </article>
+    <article>
+      <div class="article--left_part">
+      </div>
+      <div class="article--right_part">
+        <div class="article--date">
+          Le 11 juillet 2013
+        </div>
+        <div class="article--content_container">
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, totam?
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae sint nam vero, repudiandae expedita odit
+            perspiciatis laboriosam cupiditate ex itaque minus aliquam minima. Quibusdam reiciendis nulla itaque totam
+            quae fuga, unde aperiam? Mollitia reiciendis accusamus, aspernatur hic, voluptatem voluptas sit alias in,
+            excepturi laboriosam beatae unde repudiandae! Harum iure adipisci culpa placeat aliquam repellat itaque?
+            Dicta deleniti officia obcaecati. Asperiores aspernatur eaque aliquam iure aperiam ut dolores dolore, nemo
+            maiores laudantium fugit vero delectus facere ab repellendus commodi. Officiis recusandae facere quidem
+            expedita perspiciatis soluta incidunt. Officiis omnis accusantium at quis quos nam iusto possimus molestias
+            ex? Officia, optio deleniti!
+          </p>
+        </div>
+        <div class="article--footer">
+        <div class="article--category">
+          Catégorie 1
+        </div>
+        <div class="article--category">
+          Catégorie 2
+        </div>
+        <a href="https://ha-ha.space/" target="_blank" class="article--social_media" ><img src="../images/facebook.png" alt="facebook_icon"></a>
+        <a href="https://ha-ha.space/" target="_blank" class="article--social_media" ><img src="../images/twitter.png" alt="twitter_icon"></a>
+        </div>
       </div>
     </article>
     <div class="button_container">
@@ -68,12 +193,13 @@
       <p>Design agence wype</p>
     </div>
     <ul class="footer--links">
-      <li>Mentions légales</li>
-      <li>Conditions générales</li>
-      <li>Presse</li>
-      <li>Contact</li>
+      <a href="https://ha-ha.space/" target="_blank"><li>Mentions légales</li></a>
+      <a href="https://ha-ha.space/" target="_blank"><li>Conditions générales</li></a>
+      <a href="https://ha-ha.space/" target="_blank"><li>Presse</li></a>
+      <a href="https://ha-ha.space/" target="_blank"><li>Contact</li></a>
     </ul>
   </footer>
+  </div>
 </body>
 
 </html>
